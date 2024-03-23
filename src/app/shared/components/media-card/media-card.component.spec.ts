45f00f939f4980ad867d77314241940c1f8b356d
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-03-23 16:42:59 +0100

    refactor: media-card component tests

--- src/app/shared/components/media-card/media-card.component.spec.ts
+++ src/app/shared/components/media-card/media-card.component.spec.ts
@@ -1,127 +1,101 @@
-import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MediaCardComponent } from './media-card.component';
-import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
-
 import { RouterTestingModule } from '@angular/router/testing';
 import { TextCutPipe } from '@app/shared/pipes/text-cut.pipe';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MediaType } from '@app/shared/models/media.type';
-
-@Component({
-  template: `<app-media-card
-    [mediaType]="mediaType"
-    [imagePath]="imagePath"
-    [voteAverage]="voteAverage"
-    [releaseDate]="releaseDate"
-    [title]="title"
-    [id]="id"
-    [titleSize]="titleSize"
-    [textCut]="textCut"
-    [language]="language"
-  ></app-media-card>`,
-})
-class TestHostComponent implements Partial<MediaCardComponent> {
-  mediaType!: MediaType;
-  imagePath: string | null = '';
-  voteAverage? = 0;
-  releaseDate? = '';
-  title = '';
-  id = 0;
-  titleSize?: number;
-  textCut?: number;
-  language?: string;
-}
+import { getElementById } from 'src/testing';
+import { RouterLink } from '@angular/router';
 
 describe('MediaCardComponent', () => {
-  let fixture: ComponentFixture<TestHostComponent>;
+  let fixture: ComponentFixture<MediaCardComponent>;
   let component: MediaCardComponent;
-  let hostComponent: TestHostComponent;
-  let hostElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MediaCardComponent, TestHostComponent, TextCutPipe],
+      declarations: [MediaCardComponent, TextCutPipe],
       imports: [MatCardModule, RouterTestingModule, MatTooltipModule],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TestHostComponent);
-    hostComponent = fixture.componentInstance;
-    hostElement = fixture.debugElement.query(By.directive(MediaCardComponent));
-    component = hostElement.injector.get(MediaCardComponent);
+    fixture = TestBed.createComponent(MediaCardComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render router links with correct attributes', () => {
-    const id = 123;
-    const type: MediaType = 'movie';
-    const language = 'en';
-
-    hostComponent.id = id;
-    hostComponent.mediaType = type;
+  it('should render person-card if mediaType is person', () => {
+    component.mediaType = 'person';
     fixture.detectChanges();
 
-    const linkEls = hostElement.queryAll(By.css('[data-testid="router-link"]'));
-
-    linkEls.forEach((el) => {
-      expect(el.attributes['href']).toBe(`/details/${type}/${id}`);
-    });
-
-    hostComponent.language = 'en';
-    fixture.detectChanges();
-
-    linkEls.forEach((el) => {
-      expect(el.attributes['href']).toBe(
-        `/details/${type}/${id}?language=${language}`
-      );
-    });
-  });
-
-  it('should render person-card when mediaType is a person', () => {
-    hostComponent.mediaType = 'person';
-    fixture.detectChanges();
-
-    const cardEl = hostElement.query(By.css('[data-testid="person-card"]'));
+    const cardEl = getElementById(fixture, 'person-card');
 
     expect(cardEl).toBeTruthy();
   });
 
-  it('should render show-card when mediaType is not a person', () => {
-    hostComponent.mediaType = 'movie';
+  it('should render show-card if mediaType is not person', () => {
+    component.mediaType = 'movie';
     fixture.detectChanges();
 
-    const cardEl = hostElement.query(By.css('[data-testid="show-card"]'));
+    const cardEl = getElementById(fixture, 'show-card');
 
-    expect(hostComponent.mediaType).not.toBe('person');
     expect(cardEl).toBeTruthy();
+    expect(component.mediaType).not.toBe('person');
+  });
+
+  it('should render routerLinks correctly', () => {
+    component.id = 123;
+    component.mediaType = 'movie';
+    component.title = 'title';
+    fixture.detectChanges();
+
+    const linkEls = fixture.debugElement.queryAll(By.directive(RouterLink));
+
+    expect(linkEls[0].attributes['href']).toBe('/details/movie/123');
+    expect(linkEls[1].attributes['href']).toBe('/details/movie/123');
+  });
+
+  it('should render routerLinks with language if provided', () => {
+    component.id = 123;
+    component.mediaType = 'movie';
+    component.title = 'title';
+    component.language = 'eng';
+    fixture.detectChanges();
+
+    const linkEls = fixture.debugElement.queryAll(By.directive(RouterLink));
+
+    expect(linkEls[0].attributes['href']).toBe(
+      '/details/movie/123?language=eng'
+    );
+    expect(linkEls[1].attributes['href']).toBe(
+      '/details/movie/123?language=eng'
+    );
   });
 
   it('should cut title if textCut input is provided', () => {
-    hostComponent.title = 'title example';
-    hostComponent.textCut = 5;
-    hostComponent.mediaType = 'movie';
+    component.title = 'title example';
+    component.textCut = 5;
+    component.mediaType = 'movie';
 
     fixture.detectChanges();
 
-    const titleEl = hostElement.query(By.css('.card__title'));
+    const titleEl = getElementById(fixture, 'title');
 
-    expect(titleEl.nativeElement.textContent).toBe('title...');
+    expect(titleEl.nativeElement.textContent.trim()).toBe('title...');
   });
 
   it('should set title if textCut input is not provided ', () => {
     const title = 'title example';
 
-    hostComponent.title = title;
-    hostComponent.mediaType = 'movie';
+    component.title = title;
+    component.mediaType = 'person';
 
     fixture.detectChanges();
 
-    const titleEl = hostElement.query(By.css('.card__title'));
+    const titleEl = getElementById(fixture, 'title');
 
-    expect(titleEl.nativeElement.textContent).toBe(title);
+    expect(titleEl.nativeElement.textContent.trim()).toBe(title);
   });
 });
