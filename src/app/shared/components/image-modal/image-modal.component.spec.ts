4551bf0ebf90740b31d58202dcc9c9cfb59f57bf
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-03-23 16:06:28 +0100

    refactor: image-modal component tests

--- src/app/shared/components/image-modal/image-modal.component.spec.ts
+++ src/app/shared/components/image-modal/image-modal.component.spec.ts
@@ -1,16 +1,11 @@
-import { DebugElement } from '@angular/core';
 import { ImageModalComponent } from './image-modal.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { By } from '@angular/platform-browser';
-import { mockImgData } from 'src/testing';
+import { getElementById, mockImgData } from 'src/testing';
 
 describe('ImageModalComponent', () => {
   let fixture: ComponentFixture<ImageModalComponent>;
   let component: ImageModalComponent;
-  let element: DebugElement;
-
-  let imgEl: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,23 +24,19 @@ describe('ImageModalComponent', () => {
 
     fixture = TestBed.createComponent(ImageModalComponent);
     component = fixture.componentInstance;
-    element = fixture.debugElement;
 
     fixture.detectChanges();
-
-    imgEl = element.query(By.css('[data-testid="img"]'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render img element', () => {
-    expect(imgEl).toBeTruthy();
-  });
+  it('should render correctly dialog', () => {
+    const imgEl = getElementById(fixture, 'img');
 
-  it('should render img element with injected data properties', () => {
-    expect(imgEl.nativeElement.src).toBe(mockImgData.src);
-    expect(imgEl.nativeElement.alt).toBe(mockImgData.alt);
+    expect(imgEl).toBeTruthy();
+    expect(imgEl.attributes['src']).toBe(mockImgData.src);
+    expect(imgEl.attributes['alt']).toBe(mockImgData.alt);
   });
 });
